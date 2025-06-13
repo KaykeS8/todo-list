d3077f0082218119810df20bedc995a89a53c9f5
Kayke simao <kayke.py@gmail.com>
2025-06-12 21:21:23 -0300

    Schema task

---
+++ lib/todo_list/tasks/task.ex
@@ -0,0 +1,21 @@
+defmodule TodoList.Tasks.Task do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "tasks" do
+    field :title, :string
+    field :description, :string
+    field :status, :string
+    field :user_id, :id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(task, attrs, user_scope) do
+    task
+    |> cast(attrs, [:title, :description, :status])
+    |> validate_required([:title, :description, :status])
+    |> put_change(:user_id, user_scope.user.id)
+  end
+end
